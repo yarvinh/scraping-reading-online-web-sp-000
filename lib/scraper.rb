0afd0f4b094f06dfb2cd8ce863e6079cd5af9df9
Yarvin Hernandez <yarhumher@hotmail.com>
2020-04-03 22:16:22 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,18 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+# html = open("https://flatironschool.com/")
+require 'nokogiri'
+require 'open-uri'
+
+#  html = open("https://flatironschool.com/")
+#  Nokogiri::HTML(html)
+
+ doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+#   p doc.css(".headline-26OIBN")#.text#.strip
+html_array =  doc.css("#f2741bde-8219-55b6-9f40-9f197188ccc0")
+
+#  html_array.each {|html|
+#  #Operating on XML
+#  puts html.text.strip}
+p html_array[0].attributes
